5d75550c6882a2fcd9985bd43abf0ade65278a64
KaleKemosabe <kale.kemosabe@gmail.com>
2020-10-26 21:09:44 +0200

    comments added to js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ let gravity = 0.9;
 let isGameOver = false;
 
 function control(e) {
+// character jump on spacebar
     if (e.keyCode === 32) {
         if (!isJumping) {
             isJumping === true;
             jump()
         }
-// check out keycodes -- e.g. 32 is spacebar
     }
 }
 document.addEventListener('keyup', control);
@@ -21,53 +21,54 @@ let position = 0;
 function jump() {
     let count = 0;
     let timerId = setInterval(function () {
-        // move down
-        if (count === 15) {
+// dude moving down after jump
+        if (count === 20) {
             clearInterval(timerId);
-            console.log('down');
             let downTimerId = setInterval(function () {
                 if (count === 0) {
                     clearInterval(downTimerId);
                     isJumping === false;
                 }
-                position -= 5;
+// character not falling underground or floating when position approx -= 2
+                position -= 2;
                 count--;
                 position = position * gravity;
                 dude.style.bottom = position + 'px';
-            },20)
+            },25)
         }
-        //move up
-        console.log('up');
-        position +=30;
+// dude moving up on jump
+        position +=20;
         count++;
         position = position * gravity;
         dude.style.bottom = position + 'px';
         console.log(dude.style.bottom);
     },20)
 }
-
+// create obstacles, define speed, position etc
 function generateObstacles() {
-    let randomTime = Math.random() * 4000;
-    let obstaclePosition = 1000;
+    let randomTime = Math.random() * 3000;
+    let obstaclePosition = 1500;
     const obstacle = document.createElement('div');
     if (!isGameOver) obstacle.classList.add('obstacle');
     grid.appendChild(obstacle);
     obstacle.style.left = obstaclePosition + 'px'
 
     let timerId = setInterval(function() {
-        if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
+// when crashing to character
+        if (obstaclePosition > 0 && obstaclePosition < 100 && position < 100) {
             clearInterval(timerId);
-            alert.innerHTML = 'Game over'
+// game over message
+            alert.innerHTML = 'Dude, you suck!';
             isGameOver = true;
-            // remove all children
+// remove all children when crashing on character
             while (grid.firstChild) {
                 grid.removeChild(grid.lastChild);
             }
         }
-
-        obstaclePosition -= 10;
+// change obstaclePosition to 10 for slower version
+        obstaclePosition -= 15;
         obstacle.style.left = obstaclePosition + 'px';
-    },15)
+    },20)
     if (!isGameOver) setTimeout(generateObstacles, randomTime);
 }
 generateObstacles();
